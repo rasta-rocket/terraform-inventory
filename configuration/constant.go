b6c7105e085b1e1d0233b54122fda6a152e4b264
rasta-rocket <bruno.fernandos@yahoo.com>
2017-11-28 14:53:39 +0100

    Rename inventory output filename

--- configuration/constant.go
+++ configuration/constant.go
@@ -1,6 +1,6 @@
 package configuration
 
 const DEFAULT_TFSTATE_LOCATION = "./terraform.tfstate"
-const DEFAULT_OUTPUT_FILE = "./inventory.test"
+const DEFAULT_OUTPUT_FILE = "./hosts"
 const DEFAULT_SSH_USER = "SSH_USER"
 const DEFAULT_SSH_KEY = "SSH_KEY"
