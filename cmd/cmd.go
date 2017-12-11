1a0ef2af55e9cc56b38aab453a7152b0a1466a5f
rasta-rocket <bruno.fernandos@yahoo.com>
2017-12-11 11:47:58 +0100

    Add version flag to cmd

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,15 +1,24 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/rasta-rocket/terraform-inventory/configuration"
 	"github.com/rasta-rocket/terraform-inventory/openstack"
+
 	"github.com/spf13/cobra"
 )
+
+var isVersion bool
 
 var RootCmd = &cobra.Command{
 	Use:   "terraform-inventory",
 	Short: "terraform-inventory generates Ansible inventory based on Terraform deployment",
 	Run: func(cmd *cobra.Command, args []string) {
+		if isVersion {
+			displayVersion()
+			return
+		}
 		run()
 	},
 }
@@ -21,7 +30,12 @@ func run() {
 	openstack.ToInventory(set, inventory)
 }
 
+func displayVersion() {
+	fmt.Printf("terraform-inventory version %s\n", configuration.VERSION)
+}
+
 func init() {
+	RootCmd.Flags().BoolVarP(&isVersion, "version", "v", false, "version")
 	RootCmd.Flags().StringVarP(&configuration.Conf.Tfstate, "tfstate", "t", configuration.DEFAULT_TFSTATE_LOCATION, "local tfstate file")
 	RootCmd.Flags().StringVarP(&configuration.Conf.OutputFile, "output", "o", configuration.DEFAULT_OUTPUT_FILE, "ansible inventory file to output")
 	RootCmd.Flags().StringVarP(&configuration.Conf.SshUser, "ssh-user", "u", configuration.DEFAULT_SSH_USER, "ansible ssh user")
--- configuration/constant.go
+++ configuration/constant.go
@@ -1,5 +1,6 @@
 package configuration
 
+const VERSION = "1.0.0"
 const DEFAULT_TFSTATE_LOCATION = "./terraform.tfstate"
 const DEFAULT_OUTPUT_FILE = "./hosts"
 const DEFAULT_SSH_USER = "SSH_USER"
