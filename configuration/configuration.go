b3a7671644b3af8285a7f28ed87342f0661213f4
rasta-rocket <bruno.fernandos@yahoo.com>
2018-03-15 23:47:01 +0100

    Add isPathExist and getCurrentWorkspace methods

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -1,10 +1,13 @@
 package configuration
 
 import (
+	"io/ioutil"
 	"os"
 )
 
 type Configuration struct {
+	TfPath     string
+	Workspace  string
 	Tfstate    string
 	OutputFile string
 	SshUser    string
@@ -16,6 +19,24 @@ func (conf *Configuration) Init() {
 		conf.Tfstate = DEFAULT_TFSTATE_LOCATION
 	}
 	conf.OutputFile = DEFAULT_OUTPUT_FILE
+}
+
+func isPathExist(path string) bool {
+	_, err := os.Stat(path)
+	return os.IsExist(err)
+}
+
+func getCurrentWorkspace(path string) string {
+	workspace := "default"
+	defaultEnvironmentPath := ".terraform/environment"
+	environmentPath := path + "/" + defaultEnvironmentPath
+
+	if !isPathExist(environmentPath) {
+		return workspace
+	}
+
+	byteContent, _ := ioutil.ReadFile(environmentPath)
+	return string(byteContent)
 }
 
 func isLocalTfstateExist() bool {
